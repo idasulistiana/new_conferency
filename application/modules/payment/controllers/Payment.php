d6ab75cf3cda7d60401bf4cf8fb72cdd9776528e
Nastiti Arum Puspawati <nastitiarump@gmail.com>
2016-08-25 15:21:40 +0700

    modifikasi payment

--- application/modules/payment/controllers/Payment.php
+++ application/modules/payment/controllers/Payment.php
@@ -26,14 +26,17 @@ class Payment extends CI_Controller {
 		$this->Payment_m->setMessage($this->input->post('message'));
 		$this->Payment_m->setIdStatus($this->input->post('category'));
 		$this->Payment_m->setSatuan($this->input->post('satuan'));
-		$this->payment_m->setIdPaper($this->input->post('paperid'));
+		
 		$regis = $this->Payment_m->cek_idregis();
 		if($regis){
 			$this->Payment_m->setIdRegis($regis);
+			if($this->input->post('category') == 2){
+				$this->payment_m->setPaper($this->input->post('paperid'));
+			}
 			$this->Payment_m->tambah();
 			$this->session->set_flashdata('sukses', 'Thank you. For more information please contact us at icbbogor2016@gmail.com');
 		}else{
-			$this->session->set_flashdata('error', 'You must registration before!');
+			$this->session->set_flashdata('error', 'Incorrect full name or email address. Please registration.');
 			$this->session->set_flashdata('fName', $this->input->post('fName'));
 			$this->session->set_flashdata('lName',$this->input->post('lName'));
 			$this->session->set_flashdata('company',$this->input->post('company'));
